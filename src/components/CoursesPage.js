43ac6beb891696984e9ae963213dcdd270e6dc53
Tyus Durant <contact@tyusdurant.com>
2019-09-16 16:25:26 -0400

    endofday0916

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -1,20 +1,28 @@
-import React, { useState, useEffect } from 'react'
-import { getCourses } from "../api/courseApi";
-import CoursesList from './CourseList';
+import React, { useState, useEffect } from "react";
+import courseStore from "../stores/courseStore";
+import CoursesList from "./CourseList";
+import { loadCourses, deleteCourse } from "../actions/courseActions";
 import { Link } from "react-router-dom";
-const CoursesPage = () => {
-    const [courses, setCourses] = useState([]);
+function CoursesPage() {
+  const [courses, setCourses] = useState(courseStore.getCourses());
 
-    useEffect(() => {
-        getCourses().then(_courses => setCourses(_courses));
-    }, [])
+  useEffect(() => {
+    courseStore.addChangeListener(onChange);
+    if (courseStore.getCourses().length === 0) loadCourses();
 
-    return (
-        <div>
-            <h2>Courses</h2>
-            <Link to="/course" className="btn btn-primary">Add Course</Link>
-            <CoursesList courses={courses} />
-        </div>
-    )
+    return () => courseStore.removeChangeListener(onChange);
+  }, []);
+  function onChange() {
+    setCourses(courseStore.getCourses());
+  }
+  return (
+    <div>
+      <h2>Courses</h2>
+      <Link to="/course" className="btn btn-primary">
+        Add Course
+      </Link>
+      <CoursesList courses={courses} deleteCourse={deleteCourse} />
+    </div>
+  );
 }
 export default CoursesPage;
